13bc8cc56ac9dc114af1b7d27f4e10373f099045
kiet <chungkiet31@gmail.com>
2025-09-26 03:07:20 +0700

    a

--- layers/network.py
+++ layers/network.py
@@ -2,69 +2,50 @@ import torch
 from torch import nn
 
 class Network(nn.Module):
-    def __init__(self, seq_len, pred_len, patch_len, stride, padding_patch, period_len=24, enc_in=1):
+    def __init__(self, seq_len, pred_len, patch_len, stride, padding_patch):
         super(Network, self).__init__()
 
         # Parameters
         self.pred_len = pred_len
-        self.seq_len = seq_len
-        self.period_len = period_len
-        self.enc_in = enc_in
-        self.d_model = 108
 
         # Non-linear Stream
         # Patching
         self.patch_len = patch_len
         self.stride = stride
         self.padding_patch = padding_patch
-        
-        self.dim = patch_len
+        self.dim = patch_len * patch_len
         self.patch_num = (seq_len - patch_len)//stride + 1
         if padding_patch == 'end': # can be modified to general case
             self.padding_patch_layer = nn.ReplicationPad1d((0, stride)) 
             self.patch_num += 1
-        
-        self.conv1d = nn.Conv1d(in_channels=1, out_channels=1, kernel_size=1 + 2 * (self.period_len // 2),
-                                stride=1, padding=self.period_len // 2, padding_mode="zeros", bias=False)
 
-        # self.mlp = nn.Sequential(
-        #         nn.Linear(self.seg_num_x, self.d_model),
-        #         nn.ReLU(),
-        #         nn.Linear(self.d_model, self.seg_num_y)
-        #     )
-
-        # # Patch Embedding
+        # Patch Embedding
         self.fc1 = nn.Linear(patch_len, self.dim)
         self.gelu1 = nn.GELU()
         self.bn1 = nn.BatchNorm1d(self.patch_num)
+        
+        # CNN Depthwise
+        self.conv1 = nn.Conv1d(self.patch_num, self.patch_num,
+                               patch_len, patch_len, groups=self.patch_num)
+        self.gelu2 = nn.GELU()
+        self.bn2 = nn.BatchNorm1d(self.patch_num)
 
+        # Residual Stream
+        self.fc2 = nn.Linear(self.dim, patch_len)
+
+        # CNN Pointwise
+        self.conv2 = nn.Conv1d(self.patch_num, self.patch_num, 1, 1)
+        self.gelu3 = nn.GELU()
+        self.bn3 = nn.BatchNorm1d(self.patch_num)
+
+        # Flatten Head
         self.flatten1 = nn.Flatten(start_dim=-2)
-        self.fc3 = nn.Linear(self.patch_num * self.dim, pred_len * 2)
+        self.fc3 = nn.Linear(self.patch_num * patch_len, pred_len * 2)
         self.gelu4 = nn.GELU()
         self.fc4 = nn.Linear(pred_len * 2, pred_len)
-        
-        # # CNN Depthwise
-        # self.conv1 = nn.Conv1d(self.patch_num, self.patch_num,
-        #                        patch_len, patch_len, groups=self.patch_num)
-        # self.gelu2 = nn.GELU()
-        # self.bn2 = nn.BatchNorm1d(self.patch_num)
 
-        # # Residual Stream
-        # self.fc2 = nn.Linear(self.dim, patch_len)
-
-        # # CNN Pointwise
-        # self.conv2 = nn.Conv1d(self.patch_num, self.patch_num, 1, 1)
-        # self.gelu3 = nn.GELU()
-        # self.bn3 = nn.BatchNorm1d(self.patch_num)
-
-        # # Flatten Head
-        # self.flatten1 = nn.Flatten(start_dim=-2)
-        # self.fc3 = nn.Linear(self.patch_num * patch_len, pred_len * 2)
-        # self.gelu4 = nn.GELU()
-        # self.fc4 = nn.Linear(pred_len * 2, pred_len)
-
-        # # Linear Stream
-        # # MLP
+        # Linear Stream
+        # MLP
         self.fc5 = nn.Linear(seq_len, pred_len * 4)
         self.avgpool1 = nn.AvgPool1d(kernel_size=2)
         self.ln1 = nn.LayerNorm(pred_len * 2)
@@ -82,47 +63,61 @@ class Network(nn.Module):
         # x: [Batch, Input, Channel]
         # s - seasonality
         # t - trend
-
-        batch_size = s.shape[0]
-        seq_mean = torch.mean(s, dim=1).unsqueeze(1)  # [B, 1, C]
-        s = (s - seq_mean).permute(0, 2, 1)           # [B, C, seq_len]
-        B, C, S = s.shape
-
+        
+        s = s.permute(0,2,1) # to [Batch, Channel, Input]
+        t = t.permute(0,2,1) # to [Batch, Channel, Input]
+        
         # Channel split for channel independence
-        s = torch.reshape(s, (B*C, S)) # [B*C, seq_len]
+        B = s.shape[0] # Batch size
+        C = s.shape[1] # Channel size
+        I = s.shape[2] # Input size
+        s = torch.reshape(s, (B*C, I)) # [Batch and Channel, Input]
+        t = torch.reshape(t, (B*C, I)) # [Batch and Channel, Input]
 
+        # Non-linear Stream
         # Patching
         if self.padding_patch == 'end':
             s = self.padding_patch_layer(s)
-        patches = s.unfold(dimension=-1, size=self.patch_len, step=self.stride) # [B*C, patch_num, patch_len]
+        s = s.unfold(dimension=-1, size=self.patch_len, step=self.stride)
+        # s: [Batch and Channel, Patch_num, Patch_len]
+        
+        # Patch Embedding
+        s = self.fc1(s)
+        s = self.gelu1(s)
+        s = self.bn1(s)
 
-        # Downsample từng patch
-        patches = patches.unsqueeze(2) # [B*C, patch_num, 1, patch_len]
-        patches = patches.reshape(-1, 1, self.patch_len) # [B*C*patch_num, 1, patch_len]
-        patches = self.conv1d(patches) # [B*C*patch_num, 1, patch_len//2]
-        patches = patches.squeeze(1)   # [B*C*patch_num, patch_len//2]
+        res = s
 
-        # Patch embedding
-        patches = self.fc1(patches)    # [B*C*patch_num, dim]
-        patches = self.gelu1(patches)
-        patches = patches.reshape(B*C, self.patch_num, self.dim)
-        patches = self.bn1(patches)
+        # CNN Depthwise
+        s = self.conv1(s)
+        s = self.gelu2(s)
+        s = self.bn2(s)
+
+        # Residual Stream
+        res = self.fc2(res)
+        s = s + res
+
+        # CNN Pointwise
+        s = self.conv2(s)
+        s = self.gelu3(s)
+        s = self.bn3(s)
 
         # Flatten Head
-        patches = self.flatten1(patches) # [B*C, patch_num * dim]
-        patches = self.fc3(patches)
-        patches = self.gelu4(patches)
-        s = self.fc4(patches)            # [B*C, pred_len]
+        s = self.flatten1(s)
+        s = self.fc3(s)
+        s = self.gelu4(s)
+        s = self.fc4(s)
 
-        # Linear Stream (trend)
-        t = t.permute(0,2,1)                               # [B, C, seq_len]
-        t = torch.reshape(t, (batch_size*self.enc_in, self.seq_len))
+        # Linear Stream
+        # MLP
         t = self.fc5(t)
         t = self.avgpool1(t)
         t = self.ln1(t)
+
         t = self.fc6(t)
         t = self.avgpool2(t)
         t = self.ln2(t)
+
         t = self.fc7(t)
 
         # Streams Concatination
@@ -131,6 +126,7 @@ class Network(nn.Module):
 
         # Channel concatination
         x = torch.reshape(x, (B, C, self.pred_len)) # [Batch, Channel, Output]
+
         x = x.permute(0,2,1) # to [Batch, Output, Channel]
 
         return x
--- models/xPatch.py
+++ models/xPatch.py
@@ -1,60 +1,128 @@
 import torch
 import torch.nn as nn
-import math
-
-from layers.decomp import DECOMP
-from layers.network import Network
-# from layers.network_mlp import NetworkMLP # For ablation study with MLP-only stream
-# from layers.network_cnn import NetworkCNN # For ablation study with CNN-only stream
 from layers.revin import RevIN
 
 class Model(nn.Module):
     def __init__(self, configs):
         super(Model, self).__init__()
 
-        # Parameters
-        seq_len = configs.seq_len   # lookback window L
-        pred_len = configs.pred_len # prediction length (96, 192, 336, 720)
-        c_in = configs.enc_in       # input channels
-        period_len = 24
+        self.seq_len = configs.seq_len
+        self.pred_len = configs.pred_len
+        self.enc_in = configs.enc_in
+        self.period_len = 24
+        self.d_model = configs.d_model
+        self.patch_len = configs.patch_len
+        self.stride = configs.stride
+        self.padding_patch = configs.padding_patch
 
+        self.patch_num = (self.seq_len - self.patch_len)//self.stride + 1
+        if self.padding_patch == 'end':
+            self.padding_patch_layer = nn.ReplicationPad1d((0, self.stride)) 
+            self.patch_num += 1
 
-        # Patching
-        patch_len = configs.patch_len
-        stride = configs.stride
-        padding_patch = configs.padding_patch
+        # RevIN layer
+        self.revin_layer = RevIN(self.enc_in, affine=True, subtract_last=False)
 
-        # Normalization
-        self.revin = configs.revin
-        self.revin_layer = RevIN(c_in,affine=True,subtract_last=False)
+        # Patch Embedding
+        self.fc1 = nn.Linear(self.patch_len, self.d_model)
+        self.gelu1 = nn.GELU()
+        self.bn1 = nn.BatchNorm1d(self.patch_num)
 
-        # Moving Average
-        self.ma_type = configs.ma_type
-        alpha = configs.alpha       # smoothing factor for EMA (Exponential Moving Average)
-        beta = configs.beta         # smoothing factor for DEMA (Double Exponential Moving Average)
+        # CNN Depthwise
+        self.conv1 = nn.Conv1d(self.patch_num, self.patch_num, self.patch_len, self.patch_len, groups=self.patch_num)
+        self.gelu2 = nn.GELU()
+        self.bn2 = nn.BatchNorm1d(self.patch_num)
 
-        self.decomp = DECOMP(self.ma_type, alpha, beta)
-        self.net = Network(seq_len, pred_len, patch_len, stride, padding_patch, period_len, c_in)
-        # self.net_mlp = NetworkMLP(seq_len, pred_len) # For ablation study with MLP-only stream
-        # self.net_cnn = NetworkCNN(seq_len, pred_len, patch_len, stride, padding_patch) # For ablation study with CNN-only stream
+        # Residual Stream
+        self.fc2 = nn.Linear(self.d_model, self.patch_len)
+
+        # CNN Pointwise
+        self.conv2 = nn.Conv1d(self.patch_num, self.patch_num, 1, 1)
+        self.gelu3 = nn.GELU()
+        self.bn3 = nn.BatchNorm1d(self.patch_num)
+
+        # Flatten Head
+        self.flatten1 = nn.Flatten(start_dim=-2)
+        self.fc3 = nn.Linear(self.patch_num * self.patch_len, self.seq_len)
+        self.gelu4 = nn.GELU()
+        self.fc4 = nn.Linear(self.seq_len, self.seq_len)
+
+        # Downsampling (aggregation theo period_len)
+        self.conv1d = nn.Conv1d(
+            in_channels=1, out_channels=1,
+            kernel_size=1 + 2 * (self.period_len // 2),
+            stride=1, padding=self.period_len // 2,
+            padding_mode="zeros", bias=False
+        )
+
+        self.mlp = nn.Sequential(
+            nn.Linear(self.seq_len // self.period_len, self.d_model),
+            nn.GELU(),
+            nn.Linear(self.d_model, self.pred_len // self.period_len)
+        )
 
     def forward(self, x):
-        # x: [Batch, Input, Channel]
+        batch_size = x.shape[0]
 
-        # Normalization
-        if self.revin:
-            x = self.revin_layer(x, 'norm')
+        # RevIN normalization
+        x = self.revin_layer(x, 'norm')
 
-        if self.ma_type == 'reg':   # If no decomposition, directly pass the input to the network
-            x = self.net(x, x)
-            # x = self.net_mlp(x) # For ablation study with MLP-only stream
-            # x = self.net_cnn(x) # For ablation study with CNN-only stream
-        else:
-            seasonal_init, trend_init = self.decomp(x)
-            x = self.net(seasonal_init, trend_init)
+        seq_mean = torch.mean(x, dim=1).unsqueeze(1)
+        x = (x - seq_mean).permute(0, 2, 1)  # [B, C, S]
+        B, C, S = x.shape
+        x = torch.reshape(x, (B*C, S))       # [B*C, S]
 
-        # Denormalization
-        if self.revin:
-            x = self.revin_layer(x, 'denorm')
+        # Patching
+        if self.padding_patch == 'end':
+            x = self.padding_patch_layer(x)
+        x = x.unfold(dimension=-1, size=self.patch_len, step=self.stride) # [B*C, patch_num, patch_len]
+
+        # Patch Embedding + Local Feature Extraction
+        x = self.fc1(x)
+        x = self.gelu1(x)
+        x = self.bn1(x)
+
+        res = x
+
+        # CNN Depthwise
+        x = self.conv1(x)
+        x = self.gelu2(x)
+        x = self.bn2(x)
+
+        # Residual Stream
+        res = self.fc2(res)
+        x = x + res
+
+        # CNN Pointwise
+        x = self.conv2(x)
+        x = self.gelu3(x)
+        x = self.bn3(x)
+
+        # Flatten Head
+        x = self.flatten1(x)
+        x = self.fc3(x)
+        x = self.gelu4(x)
+        x = self.fc4(x)
+
+        # Downsampling theo period_len
+        x = x.reshape(-1, 1, self.seq_len)
+        x = self.conv1d(x).reshape(-1, self.enc_in, self.seq_len) + x.reshape(-1, self.enc_in, self.seq_len)
+
+        # downsampling: b,c,s -> bc,n,w -> bc,w,n
+        seg_num_x = self.seq_len // self.period_len
+        seg_num_y = self.pred_len // self.period_len
+        x = x.reshape(-1, seg_num_x, self.period_len).permute(0, 2, 1)
+
+        # Qua MLP để dự đoán
+        x = self.mlp(x)  # [B*C, period_len, seg_num_y]
+
+        # upsampling: bc,w,n -> bc,n,w -> b,c,s
+        x = x.permute(0, 2, 1).reshape(batch_size, self.enc_in, self.pred_len)
+
+        # permute and denorm
+        x = x.permute(0, 2, 1) + seq_mean
+
+        # RevIN denormalization
+        x = self.revin_layer(x, 'denorm')
 
         return x
