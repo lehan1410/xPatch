dfc1d181aec971f453c65ca4a478f022b163722a
kiet <chungkiet31@gmail.com>
2025-09-27 23:37:58 +0700

    a

--- layers/network.py
+++ layers/network.py
@@ -52,7 +52,7 @@ class Network(nn.Module):
             padding=self.period_len // 2
         )
 
-        self.mixer = MLPMixerBlock(self.period_len, self.seg_num_x, hidden_dim=16)
+        # self.mixer = MLPMixerBlock(self.period_len, self.seg_num_x, hidden_dim=16)
 
 
         self.mlp = nn.Sequential(
@@ -62,11 +62,18 @@ class Network(nn.Module):
         )
 
         # Linear Stream
-        self.fc5 = nn.Linear(seq_len, pred_len * 2)
-        self.gelu1 = nn.GELU()
-        self.ln1 = nn.LayerNorm(pred_len * 2)
-        self.fc7 = nn.Linear(pred_len * 2, pred_len)
-        self.fc8 = nn.Linear(pred_len, pred_len)
+        self.linear_stream = nn.Sequential(
+            nn.Linear(seq_len, seq_len // 2),
+            nn.GELU(),
+            nn.Linear(seq_len // 2, seq_len // 4),
+            nn.GELU(),
+            nn.Linear(seq_len // 4, pred_len * 2),
+            nn.GELU(),
+            nn.LayerNorm(pred_len * 2),
+            nn.Linear(pred_len * 2, pred_len),
+            nn.GELU(),
+            nn.Linear(pred_len, pred_len)
+        )
 
     def forward(self, s, t):
         # s: [Batch, Input, Channel]
@@ -85,18 +92,14 @@ class Network(nn.Module):
         s_concat = s_conv + s_pool
         s_concat = s_concat.reshape(-1, self.enc_in, self.seq_len) + s
         s = s_concat.reshape(-1, self.seg_num_x, self.period_len).permute(0, 2, 1)
-        s = self.mixer(s)
+        # s = self.mixer(s)
         y = self.mlp(s)
         y = y.permute(0, 2, 1).reshape(B, self.enc_in, self.pred_len)
         y = y.permute(0, 2, 1) # [B, pred_len, enc_in]
 
 
         # Linear Stream
-        t = self.fc5(t)
-        t = self.gelu1(t)
-        t = self.ln1(t)
-        t = self.fc7(t)
-        t = self.fc8(t)
+        t = self.linear_stream(t)
         t = torch.reshape(t, (B, C, self.pred_len))
         t = t.permute(0,2,1) # [Batch, Output, Channel] = [B, pred_len, C]
 
