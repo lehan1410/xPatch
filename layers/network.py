7ebb5f229de2a2e5ccdb167ffa6f775ecbff57c1
kiet <chungkiet31@gmail.com>
2025-09-27 16:44:55 +0700

    a

--- layers/network.py
+++ layers/network.py
@@ -1,44 +1,30 @@
 import torch
 from torch import nn
 
-class TCNBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, dilation=1):
-        super().__init__()
-        self.conv = nn.Conv1d(
-            in_channels, out_channels, kernel_size,
-            padding=(kernel_size-1)*dilation//2, dilation=dilation
-        )
-        self.bn = nn.BatchNorm1d(out_channels)
-        self.relu = nn.GELU()
-        self.residual = nn.Conv1d(in_channels, out_channels, 1) if in_channels != out_channels else nn.Identity()
-
-    def forward(self, x):
-        out = self.conv(x)
-        out = self.bn(out)
-        out = self.relu(out)
-        res = self.residual(x)
-        return out + res
-
 class Network(nn.Module):
-    def __init__(self, seq_len, pred_len, patch_len, stride, padding_patch, c_in, cycle_len):
+    def __init__(self, seq_len, pred_len, patch_len, stride, padding_patch, c_in):
         super(Network, self).__init__()
 
+        # Parameters
         self.pred_len = pred_len
         self.seq_len = seq_len
         self.enc_in  = c_in
         self.period_len = 24
         self.d_model = 128
-        self.cycle_len = cycle_len
 
         self.seg_num_x = self.seq_len // self.period_len
         self.seg_num_y = self.pred_len // self.period_len
 
-        self.temporalQuery = nn.Parameter(torch.zeros(24, self.enc_in), requires_grad=True)
-
-        # TCN thay cho Conv1d
-        self.tcn = nn.Sequential(
-            TCNBlock(1, 1, kernel_size=3, dilation=1),
-            TCNBlock(1, 1, kernel_size=3, dilation=2)
+        self.conv1d = nn.Conv1d(
+            in_channels=1, out_channels=1,
+            kernel_size=1 + 2 * (self.period_len // 2),
+            stride=1, padding=self.period_len // 2,
+            padding_mode="zeros", bias=False
+        )
+        self.pool = nn.AvgPool1d(
+            kernel_size=1 + 2 * (self.period_len // 2),
+            stride=1,
+            padding=self.period_len // 2
         )
 
         self.mlp = nn.Sequential(
@@ -55,7 +41,7 @@ class Network(nn.Module):
         self.fc7 = nn.Linear(pred_len * 2, pred_len)
         self.fc8 = nn.Linear(pred_len, pred_len)
 
-    def forward(self, s, t, cycle_index):
+    def forward(self, s, t):
         # s: [Batch, Input, Channel]
         # t: [Batch, Input, Channel]
         s = s.permute(0,2,1) # [Batch, Channel, Input]
@@ -66,18 +52,16 @@ class Network(nn.Module):
         I = s.shape[2]
         t = torch.reshape(t, (B*C, I))
 
-        gather_index = (cycle_index.view(-1, 1) + torch.arange(self.seq_len, device=s.device).view(1, -1)) % self.cycle_len
-        temporal_emb = self.temporalQuery[gather_index]  # [B, seq_len, C]
-        temporal_emb = temporal_emb.permute(0, 2, 1)     # [B, C, seq_len]
-        s_cat = torch.cat([s, temporal_emb], dim=1)      # [B, 2*C, seq_len]
-
-        # TCN thay cho Conv1d
-        s = self.tcn(s_cat) + s  # [B, C, seq_len]
-
-        s = s.reshape(-1, self.seg_num_x, self.period_len).permute(0, 2, 1)
+        # Seasonal Stream: Conv1d + Pooling
+        s_conv = self.conv1d(s.reshape(-1, 1, self.seq_len))
+        s_pool = self.pool(s.reshape(-1, 1, self.seq_len))
+        s_concat = s_conv + s_pool
+        s_concat = s_concat.reshape(-1, self.enc_in, self.seq_len) + s
+        s = s_concat.reshape(-1, self.seg_num_x, self.period_len).permute(0, 2, 1)
         y = self.mlp(s)
         y = y.permute(0, 2, 1).reshape(B, self.enc_in, self.pred_len)
-        y = y.permute(0, 2, 1)
+        y = y.permute(0, 2, 1) # [B, pred_len, enc_in]
+
 
         # Linear Stream
         t = self.fc5(t)
