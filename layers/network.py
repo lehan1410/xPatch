59d3b48c5ab81731090024020b9efe4ceab3c81d
kiet <chungkiet31@gmail.com>
2025-09-27 17:24:11 +0700

    a

--- layers/network.py
+++ layers/network.py
@@ -27,6 +27,13 @@ class Network(nn.Module):
             padding=self.period_len // 2
         )
 
+        self.period_mlp = nn.Sequential(
+            nn.Linear(self.period_len, self.period_len),
+            nn.GELU(),
+            nn.Linear(self.period_len, self.period_len)
+        )
+
+
         self.mlp = nn.Sequential(
             nn.Linear(self.seg_num_x, self.d_model),
             nn.GELU(),
@@ -58,6 +65,7 @@ class Network(nn.Module):
         s_concat = s_conv + s_pool
         s_concat = s_concat.reshape(-1, self.enc_in, self.seq_len) + s
         s = s_concat.reshape(-1, self.seg_num_x, self.period_len).permute(0, 2, 1)
+        s = self.period_mlp(s)
         y = self.mlp(s)
         y = y.permute(0, 2, 1).reshape(B, self.enc_in, self.pred_len)
         y = y.permute(0, 2, 1) # [B, pred_len, enc_in]
