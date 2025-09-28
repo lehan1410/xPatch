a08f58b42866db9d6166198f770cac171d5864a8
kiet <chungkiet31@gmail.com>
2025-09-28 15:14:22 +0700

    a

--- layers/network.py
+++ layers/network.py
@@ -1,44 +1,44 @@
 import torch
 from torch import nn
 
+class PeriodGLUBlock(nn.Module):
+    def __init__(self, period_len, num_period):
+        super().__init__()
+        self.linear = nn.Linear(num_period, num_period * 2)
+
+    def forward(self, x):
+        # x: [B, period_len, num_period]
+        out = self.linear(x)  # [B, period_len, num_period*2]
+        a, b = out.chunk(2, dim=-1)
+        return a * torch.sigmoid(b)
+
 class Network(nn.Module):
-    def __init__(self, seq_len, pred_len, c_in, period_len, d_model):
+    def __init__(self, seq_len, pred_len, patch_len, stride, padding_patch, c_in):
         super(Network, self).__init__()
 
         # Parameters
         self.pred_len = pred_len
         self.seq_len = seq_len
         self.enc_in  = c_in
-        self.period_len = period_len
-        self.d_model = d_model
+        self.period_len = 24
+        self.d_model = 128
 
         self.seg_num_x = self.seq_len // self.period_len
         self.seg_num_y = self.pred_len // self.period_len
 
-        self.conv1d_1 = nn.Conv1d(
+        self.conv1d = nn.Conv1d(
             in_channels=1, out_channels=1,
             kernel_size=1 + 2 * (self.period_len // 2),
             stride=1, padding=self.period_len // 2,
-            dilation=1, padding_mode="zeros", bias=False
+            padding_mode="zeros", bias=False
         )
-        self.conv1d_2 = nn.Conv1d(
-            in_channels=1, out_channels=1,
-            kernel_size=1 + 2 * (self.period_len // 2),
-            stride=1, padding=self.period_len // 2,
-            dilation=2, padding_mode="zeros", bias=False
-        )
-        self.conv1d_3 = nn.Conv1d(
-            in_channels=1, out_channels=1,
-            kernel_size=1 + 2 * (self.period_len // 2),
-            stride=1, padding=self.period_len // 2 * 2,
-            dilation=4, padding_mode="zeros", bias=False
-        )
-
         self.pool = nn.AvgPool1d(
             kernel_size=1 + 2 * (self.period_len // 2),
             stride=1,
             padding=self.period_len // 2
         )
+
+        self.period_glu = PeriodGLUBlock(self.period_len, self.seg_num_x)
 
         self.mlp = nn.Sequential(
             nn.Linear(self.seg_num_x, self.d_model),
@@ -47,11 +47,18 @@ class Network(nn.Module):
         )
 
         # Linear Stream
-        self.fc5 = nn.Linear(seq_len, pred_len * 2)
-        self.gelu1 = nn.GELU()
-        self.ln1 = nn.LayerNorm(pred_len * 2)
-        self.fc7 = nn.Linear(pred_len * 2, pred_len)
-        self.fc8 = nn.Linear(pred_len, pred_len)
+        self.linear_stream = nn.Sequential(
+            nn.Linear(seq_len, seq_len // 2),
+            nn.GELU(),
+            nn.Linear(seq_len // 2, seq_len // 4),
+            nn.GELU(),
+            nn.Linear(seq_len // 4, pred_len * 2),
+            nn.GELU(),
+            nn.LayerNorm(pred_len * 2),
+            nn.Linear(pred_len * 2, pred_len),
+            nn.GELU(),
+            nn.Linear(pred_len, pred_len)
+        )
 
     def forward(self, s, t):
         # s: [Batch, Input, Channel]
@@ -63,36 +70,18 @@ class Network(nn.Module):
         C = s.shape[1]
         I = s.shape[2]
         t = torch.reshape(t, (B*C, I))
-
-        # Seasonal Stream: Conv1d + Pooling
-        s_reshaped = s.reshape(-1, 1, self.seq_len)
-        s_conv1 = self.conv1d_1(s_reshaped)
-        s_conv2 = self.conv1d_2(s_reshaped)
-        s_conv3 = self.conv1d_3(s_reshaped)
-        min_len = min(s_conv1.shape[-1], s_conv2.shape[-1], s_conv3.shape[-1])
-        s_conv1 = s_conv1[..., :min_len]
-        s_conv2 = s_conv2[..., :min_len]
-        s_conv3 = s_conv3[..., :min_len]
-        s_pool = self.pool(s_reshaped)[..., :min_len]
-        s_conv = s_conv1 + s_conv2 + s_conv3
+        s_conv = self.conv1d(s.reshape(-1, 1, self.seq_len))
+        s_pool = self.pool(s.reshape(-1, 1, self.seq_len))
         s_concat = s_conv + s_pool
-        s_concat = s_concat.reshape(-1, self.enc_in, min_len)
-        s = s_concat + s[..., :min_len]
-        s = s.reshape(-1, self.seg_num_x, self.period_len).permute(0, 2, 1)
+        s_concat = s_concat.reshape(-1, self.enc_in, self.seq_len) + s
+        s = s_concat.reshape(-1, self.seg_num_x, self.period_len).permute(0, 2, 1)  # [B, period_len, num_period]
+        s = self.period_glu(s)  # GLU block
         y = self.mlp(s)
-        y = y.permute(0, 2, 1)  # [B*enc_in, seg_num_y, period_len]
-        y = y.reshape(B, self.enc_in, self.seg_num_y, self.period_len)
-        y = y.permute(0, 1, 3, 2).contiguous()  # [B, enc_in, period_len, seg_num_y]
-        y = y.view(B, self.enc_in, self.pred_len)  # [B, enc_in, pred_len]
-        y = y.permute(0, 2, 1)  # [B, pred_len, enc_in]
-
+        y = y.permute(0, 2, 1).reshape(B, self.enc_in, self.pred_len)
+        y = y.permute(0, 2, 1)
 
         # Linear Stream
-        t = self.fc5(t)
-        t = self.gelu1(t)
-        t = self.ln1(t)
-        t = self.fc7(t)
-        t = self.fc8(t)
+        t = self.linear_stream(t)
         t = torch.reshape(t, (B, C, self.pred_len))
         t = t.permute(0,2,1) # [Batch, Output, Channel] = [B, pred_len, C]
 
