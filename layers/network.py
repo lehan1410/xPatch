ed043369083be2cb5fc6321d08b5205c11040d18
kiet <chungkiet31@gmail.com>
2025-09-28 15:06:49 +0700

    a

--- layers/network.py
+++ layers/network.py
@@ -1,27 +1,16 @@
 import torch
 from torch import nn
 
-class PeriodGLUBlock(nn.Module):
-    def __init__(self, period_len, num_period):
-        super().__init__()
-        self.linear = nn.Linear(num_period, num_period * 2)
-
-    def forward(self, x):
-        # x: [B, period_len, num_period]
-        out = self.linear(x)  # [B, period_len, num_period*2]
-        a, b = out.chunk(2, dim=-1)
-        return a * torch.sigmoid(b)
-
 class Network(nn.Module):
-    def __init__(self, seq_len, pred_len, c_in, period_len, d_model):
+    def __init__(self, seq_len, pred_len, patch_len, stride, padding_patch, c_in):
         super(Network, self).__init__()
 
         # Parameters
         self.pred_len = pred_len
         self.seq_len = seq_len
         self.enc_in  = c_in
-        self.period_len = period_len
-        self.d_model = d_model
+        self.period_len = 24
+        self.d_model = 128
 
         self.seg_num_x = self.seq_len // self.period_len
         self.seg_num_y = self.pred_len // self.period_len
@@ -30,6 +19,7 @@ class Network(nn.Module):
             in_channels=1, out_channels=1,
             kernel_size=1 + 2 * (self.period_len // 2),
             stride=1, padding=self.period_len // 2,
+            dilation=2,
             padding_mode="zeros", bias=False
         )
         self.pool = nn.AvgPool1d(
@@ -38,8 +28,6 @@ class Network(nn.Module):
             padding=self.period_len // 2
         )
 
-        self.period_glu = PeriodGLUBlock(self.period_len, self.seg_num_x)
-
         self.mlp = nn.Sequential(
             nn.Linear(self.seg_num_x, self.d_model),
             nn.GELU(),
@@ -47,18 +35,11 @@ class Network(nn.Module):
         )
 
         # Linear Stream
-        self.linear_stream = nn.Sequential(
-            nn.Linear(seq_len, seq_len // 2),
-            nn.GELU(),
-            nn.Linear(seq_len // 2, seq_len // 4),
-            nn.GELU(),
-            nn.Linear(seq_len // 4, pred_len * 2),
-            nn.GELU(),
-            nn.LayerNorm(pred_len * 2),
-            nn.Linear(pred_len * 2, pred_len),
-            nn.GELU(),
-            nn.Linear(pred_len, pred_len)
-        )
+        self.fc5 = nn.Linear(seq_len, pred_len * 2)
+        self.gelu1 = nn.GELU()
+        self.ln1 = nn.LayerNorm(pred_len * 2)
+        self.fc7 = nn.Linear(pred_len * 2, pred_len)
+        self.fc8 = nn.Linear(pred_len, pred_len)
 
     def forward(self, s, t):
         # s: [Batch, Input, Channel]
@@ -70,18 +51,24 @@ class Network(nn.Module):
         C = s.shape[1]
         I = s.shape[2]
         t = torch.reshape(t, (B*C, I))
+
+        # Seasonal Stream: Conv1d + Pooling
         s_conv = self.conv1d(s.reshape(-1, 1, self.seq_len))
         s_pool = self.pool(s.reshape(-1, 1, self.seq_len))
         s_concat = s_conv + s_pool
         s_concat = s_concat.reshape(-1, self.enc_in, self.seq_len) + s
-        s = s_concat.reshape(-1, self.seg_num_x, self.period_len).permute(0, 2, 1)  # [B, period_len, num_period]
-        s = self.period_glu(s)  # GLU block
+        s = s_concat.reshape(-1, self.seg_num_x, self.period_len).permute(0, 2, 1)
         y = self.mlp(s)
         y = y.permute(0, 2, 1).reshape(B, self.enc_in, self.pred_len)
-        y = y.permute(0, 2, 1)
+        y = y.permute(0, 2, 1) # [B, pred_len, enc_in]
+
 
         # Linear Stream
-        t = self.linear_stream(t)
+        t = self.fc5(t)
+        t = self.gelu1(t)
+        t = self.ln1(t)
+        t = self.fc7(t)
+        t = self.fc8(t)
         t = torch.reshape(t, (B, C, self.pred_len))
         t = t.permute(0,2,1) # [Batch, Output, Channel] = [B, pred_len, C]
 
