70da0c1ec92b7e608cef70ba01d2b84097e5ebc6
kiet <chungkiet31@gmail.com>
2025-09-28 17:28:52 +0700

    a

--- layers/network.py
+++ layers/network.py
@@ -36,17 +36,10 @@ class Network(nn.Module):
         )
 
         # Linear Stream
-        self.fc5 = nn.Linear(seq_len, pred_len * 4)
-        self.avgpool1 = nn.AvgPool1d(kernel_size=2)
+        self.fc5 = nn.Linear(seq_len, pred_len * 2)
+        self.gelu1 = nn.GELU()
         self.ln1 = nn.LayerNorm(pred_len * 2)
-
-        self.fc6 = nn.Linear(pred_len * 2, pred_len)
-        self.avgpool2 = nn.AvgPool1d(kernel_size=2)
-        self.ln2 = nn.LayerNorm(pred_len // 2)
-
-        self.fc7 = nn.Linear(pred_len // 2, pred_len)
-
-        # Streams Concatination
+        self.fc7 = nn.Linear(pred_len * 2, pred_len)
         self.fc8 = nn.Linear(pred_len, pred_len)
 
     def forward(self, s, t):
@@ -73,20 +66,11 @@ class Network(nn.Module):
 
         # Linear Stream
         t = self.fc5(t)
-        t = self.avgpool1(t)
+        t = self.gelu1(t)
         t = self.ln1(t)
-
-        t = self.fc6(t)
-        t = self.avgpool2(t)
-        t = self.ln2(t)
-
         t = self.fc7(t)
-
         t = self.fc8(t)
-
-        # Channel concatination
-        t = torch.reshape(t, (B, C, self.pred_len)) # [Batch, Channel, Output]
-
+        t = torch.reshape(t, (B, C, self.pred_len))
         t = t.permute(0,2,1)
 
         return t + y
