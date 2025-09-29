5f87d7594f0c8be87c20a91addd214b47d1142a9
kiet <chungkiet31@gmail.com>
2025-09-30 00:25:30 +0700

    a

--- layers/network.py
+++ layers/network.py
@@ -15,59 +15,64 @@ class Network(nn.Module):
         self.seg_num_x = self.seq_len // self.period_len
         self.seg_num_y = self.pred_len // self.period_len
 
-        self.conv1d = nn.Conv1d(
-            in_channels=1, out_channels=1,
-            kernel_size=1 + 2 * (self.period_len // 2),
-            stride=1, padding=self.period_len // 2,
-            padding_mode="zeros", bias=False
-        )
-        self.pool = nn.AvgPool1d(
-            kernel_size=1 + 2 * (self.period_len // 2),
-            stride=1,
-            padding=self.period_len // 2
-        )
+        self.conv1d = nn.ModuleList([
+                nn.Conv1d(
+                    in_channels=1, out_channels=1,
+                    kernel_size=1 + 2 * (self.period_len // 2),
+                    stride=1, padding=self.period_len // 2,
+                    padding_mode="zeros", bias=False
+                ) for _ in range(c_in)
+            ])
+        self.pool = nn.ModuleList([
+                nn.AvgPool1d(
+                    kernel_size=1 + 2 * (self.period_len // 2),
+                    stride=1,
+                    padding=self.period_len // 2
+                ) for _ in range(c_in)
+            ])
 
-        self.mlp = nn.Sequential(
-                nn.Linear(self.seg_num_x, self.d_model),
-                nn.GELU(),
-                nn.Linear(self.d_model, self.seg_num_y)
-            )
+        self.mlp = nn.ModuleList([
+                nn.Sequential(
+                    nn.Linear(self.seg_num_x, self.d_model),
+                    nn.GELU(),
+                    nn.Linear(self.d_model, self.seg_num_y)
+                ) for _ in range(c_in)
+            ])
 
         # Linear Stream
-        self.trend_mlp = nn.Sequential(
-            nn.Linear(seq_len, d_model),
-            nn.GELU(),
-            nn.Linear(d_model, d_model),
-            nn.GELU(),
-            nn.Linear(d_model, pred_len)
-        )
+        self.fc_trend = nn.ModuleList([
+                nn.Sequential(
+                    nn.Linear(seq_len, pred_len * 2),
+                    nn.GELU(),
+                    nn.LayerNorm(pred_len * 2),
+                    nn.Linear(pred_len * 2, pred_len),
+                    nn.Linear(pred_len, pred_len)
+                ) for _ in range(c_in)
+            ])
 
     def forward(self, s, t):
         # s: [Batch, Input, Channel]
         # t: [Batch, Input, Channel]
-        s = s.permute(0,2,1) # [Batch, Channel, Input]
-        t = t.permute(0,2,1) # [Batch, Channel, Input]
+        # s = s.permute(0,2,1) # [Batch, Channel, Input]
+        # t = t.permute(0,2,1) # [Batch, Channel, Input]
 
-        B = s.shape[0]
-        C = s.shape[1]
-        I = s.shape[2]
+        B, L, C = s.shape
         # t = torch.reshape(t, (B*C, I))
 
-        # Seasonal Stream: Conv1d + Pooling
-        s_conv = self.conv1d(s.reshape(-1, 1, self.seq_len))
-        s_pool = self.pool(s.reshape(-1, 1, self.seq_len))
-        s_concat = s_conv + s_pool
-        s_concat = s_concat.reshape(-1, self.enc_in, self.seq_len) + s
-        s = s_concat.reshape(-1, self.seg_num_x, self.period_len).permute(0, 2, 1)
-        y = self.mlp(s)
-        y = y.permute(0, 2, 1).reshape(B, self.enc_in, self.pred_len)
-        y = y.permute(0, 2, 1) # [B, pred_len, enc_in]
+        y = torch.zeros(B, self.pred_len, C, device=s.device, dtype=s.dtype)
+        t_out = torch.zeros(B, self.pred_len, C, device=s.device, dtype=s.dtype)
+        for i in range(C):
+            # Seasonal stream cho từng channel
+            s_i = s[:,:,i].unsqueeze(1) # [B, 1, L]
+            s_conv = self.conv1d[i](s_i)
+            s_pool = self.pool[i](s_i)
+            s_concat = s_conv + s_pool + s_i
+            s_feat = s_concat.reshape(B, self.seg_num_x, self.period_len).permute(0,2,1)
+            y_i = self.mlp[i](s_feat)
+            y[:, :, i] = y_i.permute(0,2,1).reshape(B, self.pred_len)
 
-
-        # Linear Stream
-        t = t.reshape(B*C, self.seq_len)
-        t = self.trend_mlp(t)
-        t = t.reshape(B, C, self.pred_len)
-        t = t.permute(0,2,1) # [Batch, Output, Channel] = [B, pred_len, C]
-
-        return t + y
+            # Trend stream cho từng channel
+            t_i = t[:,:,i]
+            t_i = self.fc_trend[i](t_i)
+            t_out[:,:,i] = t_i
+        return t_out + y
