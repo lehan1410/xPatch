52482ef18535a2d5075d1129ef90af1727f98362
kiet <chungkiet31@gmail.com>
2025-09-30 00:50:54 +0700

    a

--- layers/network.py
+++ layers/network.py
@@ -15,56 +15,140 @@ class Network(nn.Module):
         self.seg_num_x = self.seq_len // self.period_len
         self.seg_num_y = self.pred_len // self.period_len
 
-        self.conv1d = nn.ModuleList([
-                nn.Conv1d(
-                    in_channels=1, out_channels=1,
-                    kernel_size=1 + 2 * (self.period_len // 2),
-                    stride=1, padding=self.period_len // 2,
-                    padding_mode="zeros", bias=False
-                ) for _ in range(c_in)
-            ])
+        self.conv1d = nn.Conv1d(in_channels=1, out_channels=1, kernel_size=1 + 2 * (self.period_len // 2),
+                                stride=1, padding=self.period_len // 2, padding_mode="zeros", bias=False)
 
-        self.mlp = nn.ModuleList([
-                nn.Sequential(
-                    nn.Linear(self.seg_num_x, self.d_model),
-                    nn.GELU(),
-                    nn.Linear(self.d_model, self.seg_num_y)
-                ) for _ in range(c_in)
-            ])
+        
+        self.mlp = nn.Sequential(
+                nn.Linear(self.seg_num_x, self.d_model),
+                nn.GELU(),
+                nn.Linear(self.d_model, self.seg_num_y)
+            )
+        # Non-linear Stream
+        # Patching
+        # self.patch_len = patch_len
+        # self.stride = stride
+        # self.padding_patch = padding_patch
+        # self.dim = patch_len * patch_len
+        # self.patch_num = (seq_len - patch_len)//stride + 1
+        # if padding_patch == 'end': # can be modified to general case
+        #     self.padding_patch_layer = nn.ReplicationPad1d((0, stride)) 
+        #     self.patch_num += 1
+
+        # Patch Embedding
+        # self.fc1 = nn.Linear(patch_len, self.dim)
+        # self.gelu1 = nn.GELU()
+        # self.bn1 = nn.BatchNorm1d(self.patch_num)
+        
+        # CNN Depthwise
+        # self.conv1 = nn.Conv1d(self.patch_num, self.patch_num,
+        #                        patch_len, patch_len, groups=self.patch_num)
+        # self.gelu2 = nn.GELU()
+        # self.bn2 = nn.BatchNorm1d(self.patch_num)
+
+        # # Residual Stream
+        # self.fc2 = nn.Linear(self.dim, patch_len)
+
+        # # CNN Pointwise
+        # self.conv2 = nn.Conv1d(self.patch_num, self.patch_num, 1, 1)
+        # self.gelu3 = nn.GELU()
+        # self.bn3 = nn.BatchNorm1d(self.patch_num)
+
+        # # Flatten Head
+        # self.flatten1 = nn.Flatten(start_dim=-2)
+        # self.fc3 = nn.Linear(self.patch_num * patch_len, pred_len * 2)
+        # self.gelu4 = nn.GELU()
+        # self.fc4 = nn.Linear(pred_len * 2, pred_len)
 
         # Linear Stream
-        self.fc_trend = nn.ModuleList([
-                nn.Sequential(
-                    nn.Linear(seq_len, pred_len * 2),
-                    nn.GELU(),
-                    nn.LayerNorm(pred_len * 2),
-                    nn.Linear(pred_len * 2, pred_len),
-                    nn.Linear(pred_len, pred_len)
-                ) for _ in range(c_in)
-            ])
+        # MLP
+        self.fc5 = nn.Linear(seq_len, pred_len * 2)
+        # self.avgpool1 = nn.AvgPool1d(kernel_size=2)
+        self.gelu1 = nn.GELU()
+        self.ln1 = nn.LayerNorm(pred_len * 2)
+
+        # self.fc6 = nn.Linear(pred_len * 2, pred_len)
+        # self.avgpool2 = nn.AvgPool1d(kernel_size=2)
+        # self.ln2 = nn.LayerNorm(pred_len // 2)
+
+        self.fc7 = nn.Linear(pred_len * 2, pred_len)
+
+        # Streams Concatination
+        self.fc8 = nn.Linear(pred_len, pred_len)
 
     def forward(self, s, t):
-        # s: [Batch, Input, Channel]
-        # t: [Batch, Input, Channel]
-        # s = s.permute(0,2,1) # [Batch, Channel, Input]
-        # t = t.permute(0,2,1) # [Batch, Channel, Input]
+        # x: [Batch, Input, Channel]
+        # s - seasonality
+        # t - trend
+        
+        s = s.permute(0,2,1) # to [Batch, Channel, Input]
+        t = t.permute(0,2,1) # to [Batch, Channel, Input]
+        
+        # Channel split for channel independence
+        B = s.shape[0] # Batch size
+        C = s.shape[1] # Channel size
+        I = s.shape[2] # Input size
+        # s = torch.reshape(s, (B*C, I)) # [Batch and Channel, Input]
+        t = torch.reshape(t, (B*C, I)) # [Batch and Channel, Input]
 
-        B, L, C = s.shape
-        # t = torch.reshape(t, (B*C, I))
+        s = self.conv1d(s.reshape(-1, 1, self.seq_len)).reshape(-1, self.enc_in, self.seq_len) + s
+        s = s.reshape(-1, self.seg_num_x, self.period_len).permute(0, 2, 1)
+        y = self.mlp(s)
+        y = y.permute(0, 2, 1).reshape(B, self.enc_in, self.pred_len)
+        y = y.permute(0, 2, 1)
 
-        y = torch.zeros(B, self.pred_len, C, device=s.device, dtype=s.dtype)
-        t_out = torch.zeros(B, self.pred_len, C, device=s.device, dtype=s.dtype)
-        for i in range(C):
-            # Seasonal stream cho từng channel
-            s_i = s[:,:,i].unsqueeze(1) # [B, 1, L]
-            s_conv = self.conv1d[i](s_i)
-            s_concat = s_conv + s_i
-            s_feat = s_concat.reshape(B, self.seg_num_x, self.period_len).permute(0,2,1)
-            y_i = self.mlp[i](s_feat)
-            y[:, :, i] = y_i.permute(0,2,1).reshape(B, self.pred_len)
+        # Non-linear Stream
+        # Patching
+        # if self.padding_patch == 'end':
+        #     s = self.padding_patch_layer(s)
+        # s = s.unfold(dimension=-1, size=self.patch_len, step=self.stride)
+        # s: [Batch and Channel, Patch_num, Patch_len]
+        
+        # Patch Embedding
+        # s = self.fc1(s)
+        # s = self.gelu1(s)
+        # s = self.bn1(s)
 
-            # Trend stream cho từng channel
-            t_i = t[:,:,i]
-            t_i = self.fc_trend[i](t_i)
-            t_out[:,:,i] = t_i
-        return t_out + y
+        # res = s
+
+        # # CNN Depthwise
+        # s = self.conv1(s)
+        # s = self.gelu2(s)
+        # s = self.bn2(s)
+
+        # # Residual Stream
+        # res = self.fc2(res)
+        # s = s + res
+
+        # # CNN Pointwise
+        # s = self.conv2(s)
+        # s = self.gelu3(s)
+        # s = self.bn3(s)
+
+        # # Flatten Head
+        # s = self.flatten1(s)
+        # s = self.fc3(s)
+        # s = self.gelu4(s)
+        # s = self.fc4(s)
+
+        # Linear Stream
+        # MLP
+        t = self.fc5(t)
+        t = self.gelu1(t)
+        t = self.ln1(t)
+
+        # t = self.fc6(t)
+        # t = self.avgpool2(t)
+        # t = self.ln2(t)
+
+        t = self.fc7(t)
+
+
+        t = self.fc8(t)
+
+        # Channel concatination
+        t = torch.reshape(t, (B, C, self.pred_len)) # [Batch, Channel, Output]
+
+        t = t.permute(0,2,1) # to [Batch, Output, Channel]
+
+        return t + y
