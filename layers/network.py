c51ebd82f113135c6d46c9adf90d87f64dce584e
kiet <chungkiet31@gmail.com>
2025-10-07 10:01:12 +0700

    a

--- layers/network.py
+++ layers/network.py
@@ -2,10 +2,9 @@ import torch
 from torch import nn
 
 class Network(nn.Module):
-    def __init__(self, seq_len, pred_len, c_in, period_len, d_model):
+    def __init__(self, seq_len, pred_len, c_in, period_len, d_model, time_feat_dim=4):
         super(Network, self).__init__()
 
-        # Parameters
         self.pred_len = pred_len
         self.seq_len = seq_len
         self.enc_in  = c_in
@@ -15,7 +14,6 @@ class Network(nn.Module):
         self.seg_num_x = self.seq_len // self.period_len
         self.seg_num_y = self.pred_len // self.period_len
 
-        # Conv1d cho channel independence
         self.conv1d = nn.Conv1d(
             in_channels=self.enc_in, out_channels=self.enc_in,
             kernel_size=1 + 2 * (self.period_len // 2),
@@ -23,10 +21,12 @@ class Network(nn.Module):
             padding_mode="zeros", bias=False, groups=self.enc_in
         )
 
+        # Encode time features cho từng subsequence
+        self.time_encoder = nn.Linear(time_feat_dim, self.period_len)
+
         # Attention giữa các subsequence
         self.attn_subseq = nn.MultiheadAttention(embed_dim=self.period_len, num_heads=2, batch_first=True)
-        # Attention giữa các channel
-        self.attn_channel = nn.MultiheadAttention(embed_dim=self.enc_in, num_heads=1, batch_first=True)
+
         self.mlp = nn.Sequential(
             nn.Linear(self.seg_num_x, self.d_model * 2),
             nn.GELU(),
@@ -40,9 +40,10 @@ class Network(nn.Module):
         self.fc7 = nn.Linear(pred_len * 2, pred_len)
         self.fc8 = nn.Linear(pred_len, pred_len)
 
-    def forward(self, s, t):
+    def forward(self, s, t, seq_x_mark):
         # s: [Batch, Input, Channel]
         # t: [Batch, Input, Channel]
+        # seq_x_mark: [Batch, seq_len, time_feat_dim]
         s = s.permute(0,2,1) # [Batch, Channel, Input]
         t = t.permute(0,2,1) # [Batch, Channel, Input]
 
@@ -58,19 +59,18 @@ class Network(nn.Module):
         # Chia thành các subsequence
         s_subseq = s.reshape(-1, self.seg_num_x, self.period_len)  # [B*C, seg_num_x, period_len]
 
-        # Attention giữa các subsequence
-        s_subseq_attn, _ = self.attn_subseq(s_subseq, s_subseq, s_subseq)  # [B*C, seg_num_x, period_len]
+        # Chia time features thành subsequence
+        time_subseq = seq_x_mark.unsqueeze(1).repeat(1, C, 1, 1)  # [B, C, seq_len, time_feat_dim]
+        time_subseq = time_subseq.reshape(-1, self.seg_num_x, self.period_len, seq_x_mark.shape[-1])  # [B*C, seg_num_x, period_len, time_feat_dim]
+        # Lấy trung bình đặc trưng thời gian cho mỗi subsequence
+        time_emb = time_subseq.mean(dim=2)  # [B*C, seg_num_x, time_feat_dim]
+        # Encode thành embedding cùng chiều với period_len
+        query = self.time_encoder(time_emb)  # [B*C, seg_num_x, period_len]
 
-        # Attention giữa các channel
-        s_channel = s.reshape(B, C, self.seg_num_x, self.period_len).permute(0, 3, 2, 1)  # [B, period_len, seg_num_x, C]
-        s_channel = s_channel.reshape(-1, self.seg_num_x, C)  # [B*period_len, seg_num_x, enc_in]
-        s_channel_attn, _ = self.attn_channel(s_channel, s_channel, s_channel)  # [B*period_len, seg_num_x, enc_in]  # [B*period_len, seg_num_x, C]
-        # Đưa về [B*C, seg_num_x, period_len] để cộng với s_subseq_attn
-        s_channel_attn = s_channel_attn.permute(0, 2, 1)  # [B*period_len, C, seg_num_x]
-        s_channel_attn = s_channel_attn.reshape(B*C, self.period_len, self.seg_num_x).permute(0, 2, 1)  # [B*C, seg_num_x, period_len]
+        # Attention giữa các subsequence, dùng time embedding làm query
+        s_subseq_attn, _ = self.attn_subseq(query, s_subseq, s_subseq)  # [B*C, seg_num_x, period_len]
 
-        # Cộng attention lại
-        s = s_subseq_attn + s_channel_attn
+        s = s_subseq_attn
 
         s = s.permute(0, 2, 1)  # [B*C, period_len, seg_num_x]
         s = s.reshape(-1, self.seg_num_x)
