230a5d608476694d99cc384c9d6a52f32ea1514a
kiet <chungkiet31@gmail.com>
2025-10-09 10:09:48 +0700

    a

--- layers/network.py
+++ layers/network.py
@@ -48,7 +48,8 @@ class Network(nn.Module):
         self.out_proj = nn.Linear(self.pred_len, self.enc_in)
 
         # Linear Stream
-        self.fc5 = nn.Linear(seq_len, pred_len * 2)
+        self.fc5 = nn.Linear(seq_len, pred_len * 4)
+        self.avgpool1 = nn.AvgPool1d(kernel_size=2)
         self.gelu1 = nn.GELU()
         self.ln1 = nn.LayerNorm(pred_len * 2)
         self.fc7 = nn.Linear(pred_len * 2, pred_len)
@@ -77,6 +78,7 @@ class Network(nn.Module):
         t = t.permute(0,2,1) # [B, C, Input]
         t = torch.reshape(t, (B*C, I))
         t = self.fc5(t)
+        t = self.avgpool1(t)
         t = self.gelu1(t)
         t = self.ln1(t)
         t = self.fc7(t)
