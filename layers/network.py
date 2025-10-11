71897822e50ca1af88264fa438ce506013de654e
kiet <chungkiet31@gmail.com>
2025-10-11 12:13:53 +0700

    a

--- layers/network.py
+++ layers/network.py
@@ -37,11 +37,8 @@ class Network(nn.Module):
         self.seg_num_x = self.seq_len // self.period_len
         self.seg_num_y = self.pred_len // self.period_len
 
-        self.conv1d = CausalConvBlock(
-            d_model=1,
-            kernel_size=1 + 2 * (self.period_len // 2),
-            dropout=self.dropout, padding=self.period_len // 2
-        )
+        self.conv1d = nn.Conv1d(in_channels=1, out_channels=1, kernel_size=1 + 2 * (self.period_len // 2),
+                                stride=1, padding=self.period_len // 2, padding_mode="zeros", bias=False)
 
         self.pool = nn.AvgPool1d(
             kernel_size=1 + 2 * (self.period_len // 2),
