a4162b949b1fd6a6b3bac3ddcd4f7abceda50621
kiet <chungkiet31@gmail.com>
2025-10-11 14:18:43 +0700

    a

--- layers/network.py
+++ layers/network.py
@@ -1,23 +1,6 @@
 import torch
 from torch import nn
 
-
-class CausalConvBlock(nn.Module):
-    def __init__(self, d_model, kernel_size=5, dropout=0.0, padding=0):
-        super(CausalConvBlock, self).__init__()
-        module_list = [
-            nn.ReplicationPad1d((kernel_size - 1, kernel_size - 1)),
-            nn.Conv1d(d_model, d_model, kernel_size=kernel_size),
-            nn.LeakyReLU(negative_slope=0.01, inplace=True),
-            nn.Dropout(dropout),
-            nn.Conv1d(d_model, d_model, kernel_size=kernel_size),
-            nn.Tanh()
-        ]
-        self.causal_conv = nn.Sequential(*module_list)
-
-    def forward(self, x):
-        return self.causal_conv(x)
-
 class Network(nn.Module):
     def __init__(self, seq_len, pred_len, c_in, period_len, d_model, dropout=0.1):
         super(Network, self).__init__()
@@ -28,11 +11,6 @@ class Network(nn.Module):
         self.period_len = period_len
         self.d_model = d_model
         self.dropout = dropout
-
-        # Attention cho channel
-        # self.channel_attn = nn.MultiheadAttention(
-        #     embed_dim=self.enc_in, num_heads=1, batch_first=True
-        # )
 
         self.seg_num_x = self.seq_len // self.period_len
         self.seg_num_y = self.pred_len // self.period_len
@@ -59,7 +37,7 @@ class Network(nn.Module):
             in_channels=self.enc_in, out_channels=self.enc_in,
             kernel_size=1 + 2 * (self.period_len // 2),
             stride=1, padding=self.period_len // 2,
-            padding_mode="zeros", bias=False, groups=self.enc_in
+            padding_mode="zeros", bias=False, groups=1
         )
 
         self.pool2 = nn.AvgPool1d(
@@ -70,13 +48,16 @@ class Network(nn.Module):
 
         self.tanh = nn.Tanh()
 
+        # Attention giữa các subsequence (patch)
+        self.patch_attn = nn.MultiheadAttention(
+            embed_dim=self.period_len, num_heads=2, batch_first=True
+        )
 
         self.mlp = nn.Sequential(
             nn.Linear(self.seg_num_x, self.d_model * 2),
             nn.GELU(),
             nn.Linear(self.d_model * 2, self.seg_num_y)
         )
-
 
         # Linear Stream cho trend
         self.fc5 = nn.Linear(seq_len, pred_len * 2)
@@ -94,13 +75,6 @@ class Network(nn.Module):
         B, C, I = s.shape
         t = torch.reshape(t, (B*C, I))
 
-        # s_attn_in = s.permute(0, 2, 1)  # [B, Input, Channel]
-        # s_attn_out, _ = self.channel_attn(s_attn_in, s_attn_in, s_attn_in)  # [B, Input, Channel]
-        # s = s_attn_out.permute(0, 2, 1)
-
-        # Seasonal Stream: chỉ attention các channel
-        # s_conv = self.conv1d(s.reshape(-1, 1, self.seq_len)).reshape(-1, self.enc_in, self.seq_len) + s
-
         # Block 1: Conv1d + Pool + Activation + Residual
         s_res1 = s
         s_conv1 = self.conv1d(s)                # [B, C, seq_len]
@@ -115,12 +89,19 @@ class Network(nn.Module):
         s_out = self.tanh(s_pool2)              # [B, C, seq_len]
         s_out = s_out + s_res2                  # Residual connection
 
-        # Reshape và MLP
-        s_patch = s_out.reshape(-1, self.seg_num_x, self.period_len).permute(0, 2, 1)
-        y = self.mlp(s_patch)
-        y = y.permute(0, 2, 1).reshape(B, self.enc_in, self.pred_len)
-        y = y.permute(0, 2, 1)
+        # Reshape thành patch/subsequence
+        s_patch = s_out.reshape(B * C, self.seg_num_x, self.period_len)  # [B*C, patch_num, period_len]
 
+        # Attention giữa các subsequence
+        patch_attn_out, _ = self.patch_attn(s_patch, s_patch, s_patch)   # [B*C, patch_num, period_len]
+
+        # Đưa qua MLP để dự đoán
+        y = self.mlp(patch_attn_out)                                    # [B*C, period_len, seg_num_y]
+        y = y.permute(0, 2, 1).reshape(B, C, self.seg_num_y * self.period_len)
+        y = y[:, :, :self.pred_len]                                     # [B, C, pred_len]
+        y = y.permute(0, 2, 1)                                          # [B, pred_len, C]
+
+        # Trend Stream
         t = self.fc5(t)
         t = self.gelu1(t)
         t = self.ln1(t)
