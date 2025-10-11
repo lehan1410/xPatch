8dd1ecaeeceab34bcf9e60e8961348ff0846ccbd
kiet <chungkiet31@gmail.com>
2025-10-11 16:06:48 +0700

    a

--- layers/network.py
+++ layers/network.py
@@ -16,10 +16,10 @@ class Network(nn.Module):
         self.seg_num_y = self.pred_len // self.period_len
 
         self.conv1d = nn.Conv1d(
-            in_channels=self.enc_in, out_channels=self.enc_in,
+            in_channels=1, out_channels=1,
             kernel_size=1 + 2 * (self.period_len // 2),
             stride=1, padding=self.period_len // 2,
-            padding_mode="zeros", bias=False, groups=self.enc_in
+            padding_mode="zeros", bias=False
         )
 
         self.pool = nn.AvgPool1d(
@@ -32,21 +32,6 @@ class Network(nn.Module):
             nn.LeakyReLU(negative_slope=0.01, inplace=True),
             nn.Dropout(dropout),
         )
-
-        self.conv1d2 = nn.Conv1d(
-            in_channels=self.enc_in, out_channels=self.enc_in,
-            kernel_size=1 + 2 * (self.period_len // 2),
-            stride=1, padding=self.period_len // 2,
-            padding_mode="zeros", bias=False, groups=1
-        )
-
-        self.pool2 = nn.AvgPool1d(
-            kernel_size=1 + 2 * (self.period_len // 2),
-            stride=1,
-            padding=self.period_len // 2
-        )
-
-        self.tanh = nn.Tanh()
 
         # Attention giữa các subsequence (patch)
         self.patch_attn = nn.MultiheadAttention(
@@ -75,22 +60,14 @@ class Network(nn.Module):
         B, C, I = s.shape
         t = torch.reshape(t, (B*C, I))
 
-        # Block 1: Conv1d + Pool + Activation + Residual
-        s_res1 = s
-        s_conv1 = self.conv1d(s)                # [B, C, seq_len]
-        s_pool1 = self.pool(s_conv1)            # [B, C, seq_len]
-        s_act1 = self.activation(s_pool1)       # [B, C, seq_len]
-        s_act1 = s_act1 + s_res1                # Residual connection
+        s_conv = self.conv1d(s.reshape(-1, 1, self.seq_len)).reshape(-1, self.enc_in, self.seq_len)
+        s_pool1 = self.pool(s_conv)
+        s_act = self.activation(s_pool1)
 
-        # Block 2: Conv1d2 + Pool2 + Tanh + Residual
-        s_res2 = s_act1
-        s_conv2 = self.conv1d2(s_act1)          # [B, C, seq_len]
-        s_pool2 = self.pool2(s_conv2)           # [B, C, seq_len]
-        s_out = self.tanh(s_pool2)              # [B, C, seq_len]
-        s_out = s_out + s_res2                  # Residual connection
+        s = s_act + s  
 
         # Reshape thành patch/subsequence
-        s_patch = s_out.reshape(B * C, self.seg_num_x, self.period_len)  # [B*C, patch_num, period_len]
+        s_patch = s.reshape(B * C, self.seg_num_x, self.period_len)  # [B*C, patch_num, period_len]
 
         # Attention giữa các subsequence
         patch_attn_out, _ = self.patch_attn(s_patch, s_patch, s_patch)   # [B*C, patch_num, period_len]
