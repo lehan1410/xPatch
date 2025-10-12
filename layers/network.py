b6d7f9111d19f24da6dfb2adbbb8563ac19ab685
kiet <chungkiet31@gmail.com>
2025-10-12 16:55:15 +0700

    a

--- layers/network.py
+++ layers/network.py
@@ -53,9 +53,9 @@ class Network(nn.Module):
             nn.Dropout(dropout),
         )
 
-        self.channel_attn = nn.MultiheadAttention(
-            embed_dim=self.enc_in, num_heads=1, batch_first=True
-        )
+        # self.channel_attn = nn.MultiheadAttention(
+        #     embed_dim=self.enc_in, num_heads=1, batch_first=True
+        # )
 
         self.mixer = MixerBlock(channel=self.enc_in, seq_len=self.seq_len, d_model=self.d_model, dropout=dropout)
 
@@ -85,13 +85,13 @@ class Network(nn.Module):
         s_feat = s_act + s  # residual
 
         # Attention channel
-        s_attn_in = s_feat.permute(0, 2, 1)  # [B, seq_len, C]
-        s_attn_out, _ = self.channel_attn(s_attn_in, s_attn_in, s_attn_in)
-        s_attn_out = s_attn_out.permute(0, 2, 1)  # [B, C, seq_len]
-        s_fusion = s_feat + s_attn_out  # residual
+        # s_attn_in = s_feat.permute(0, 2, 1)  # [B, seq_len, C]
+        # s_attn_out, _ = self.channel_attn(s_attn_in, s_attn_in, s_attn_in)
+        # s_attn_out = s_attn_out.permute(0, 2, 1)  # [B, C, seq_len]
+        # s_fusion = s_feat + s_attn_out  # residual
 
         # Mixer block cho các chuỗi thời gian
-        s_mixed = self.mixer(s_fusion)  # [B, C, seq_len]
+        s_mixed = self.mixer(s_feat)  # [B, C, seq_len]
 
         # Reshape thành patch/subsequence
         s_patch = s_mixed.reshape(-1, self.seg_num_x, self.period_len).permute(0, 2, 1)
